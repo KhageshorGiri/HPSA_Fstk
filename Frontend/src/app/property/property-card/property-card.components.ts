dc43efb054ac5d94a5d5ac5f7c6df44a87adaaea
KHAGESHOR GIRI <asndpmax@outlook.com>
2022-02-28 14:02:56 +0545

    Card list design

--- Frontend/src/app/property/property-card/property-card.components.ts
+++ Frontend/src/app/property/property-card/property-card.components.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core"
+import { Component, Input } from "@angular/core"
 
 @Component({   
     selector: 'app-property-card',
@@ -7,9 +7,8 @@ import { Component } from "@angular/core"
 })
 
 export class PropertyCardComponent { 
-    Property: any = {
-        "Id":1,
-        "Type": "House",
-        "Price" : 17000
-    }
+
+    @Input() property : any
+
+    
 }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+      "Id":1,
+      "Name": "The Fold house",
+      "Type": "Place",
+      "Price": 12350
+    },
+    {
+      "Id":2,
+      "Name": "The Gold house",
+      "Type": "House",
+      "Price": 15650
+    },
+    {
+      "Id":3,
+      "Name": "The Dimond house",
+      "Type": "Place",
+      "Price": 67800
+    },
+    {
+      "Id":4,
+      "Name": "The Opera house",
+      "Type": "Resident",
+      "Price": 14350
+    },
+    {
+      "Id":5,
+      "Name": "The Rocking Place",
+      "Type": "Place",
+      "Price": 133550
+    },
+    {
+      "Id":6,
+      "Name": "Red house",
+      "Type": "Place",
+      "Price": 188350
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
